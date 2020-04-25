8a009a65360673184d9eff66d26b3cf31e102d86
Anisha Jain <anishajain@outlook.com>
2020-04-25 03:19:59 -0700

    Game built and added on react site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import Unity, { UnityContent } from "react-unity-webgl";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+const unityContent = new UnityContent(
+  "Build/Ruby2DAdventureWebGL.json",
+  "Build/UnityLoader.js"
+);
+ 
+const App = () => {
+  return <div
+  style={{
+    width: "100vw",
+    height: "100%"
+  }}
+>
+<h3 style={{margin:"20px 10px"}}>Instructions: Use arrow keys to move around. Press X to talk to frog. Press C to throw cog to fix robot.</h3>
+<Unity unityContent={unityContent} /></div>;
 }
 
 export default App;
+
+
